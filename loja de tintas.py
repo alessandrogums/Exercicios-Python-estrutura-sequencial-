0d6cd55282704a191e437e1006d9e3b394e07594
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2021-09-24 10:29:57 -0300

    Update loja de tintas.py

--- loja de tintas.py
+++ loja de tintas.py
@@ -3,18 +3,33 @@
 #comprar apenas latas de 18 litros;
 #comprar apenas galões de 3,6 litros;
 #misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
-galoes = litros / 3.6
-if galoes % 3.6 != 0:
-    galoes=int(galoes) + 1
-preco2 = galoes * 25
+import math
+print('bom dia, loja de tintas. Como podemos te ajudar?')
+area=float(input('qual e o total da area a ser pintada?'))
+tinta=area/6
+lata=math.ceil(tinta/18)
+galao=math.ceil(tinta/3.6)
+preco_lata=lata*80
+preco_galao=galao*25
+preco_mistura= preco_galao + preco_lata
+escolha=int(input('voce quer comprar: 1)somente galoes(3,6L) 2)somente latas(18L) 3)mistura entre elas?'))
+if escolha == 1 :
+    quantidadef=galao
+    precof=preco_galao
+    print(f'a quantidade foi de:{quantidadef}'+ f'e o preco foi de:{precof}' )
+elif escolha==2 :
+    quantidadef=lata
+    precof=preco_lata
+    print(f'a quantidade foi de:{quantidadef}'+ f' e o preco foi de: {precof} reais')
+elif escolha==3 :
+    lata=int(tinta/18)
+    galao=math.ceil((tinta%18)/3.6)
+    precof=lata*80 + galao*25
+    if galao > 3:
+        galao=0
+        lata=int(tinta/18) + 1
+        precof=lata*80
 
-# mistura de latas e galoes
-mistura_lata = int(litros / 18.0)
-mistura_galao = int((litros - (mistura_lata * 18)) / 3.6)
+    print(f'a quantidade na mistura de galões:{galao} ' + f'e quantidade de latas e:{lata}'+'\n assim, o preco foi de:{:1f} reais'.format(precof))
 
-if litros - (mistura_lata * 18) % 3.6 != 0:
-    mistura_galao += 1
 
-print(f'Apenas latas de 18 litros:{latas}' )
-print('Apenas galões de 3.6 litros: %d' % galoes, 'preço: %d' % preco2)
-print('Mistura: %d latas e %d galoes = %.2f' % (mistura_lata, mistura_galao, ((mistura_lata * 80) + (mistura_galao * 25))))
